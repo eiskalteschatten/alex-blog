63ce82be4c98a557a502dd269f47af1b5b3a42e7
Alex <alex@alexseifert.com>
2023-10-02 22:17:42 +0200

    Cleanup

--- packages/client/src/app/shared/shared.module.ts
+++ packages/client/src/app/shared/shared.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -18,18 +16,14 @@ import { CardComponent } from './card/card.component';
   ],
   imports: [
     CommonModule,
-    MatInputModule,
     MatButtonModule,
-    MatCardModule,
     MatProgressBarModule,
     MatProgressSpinnerModule,
     FormsModule,
     ReactiveFormsModule,
   ],
   exports: [
-    MatInputModule,
     MatButtonModule,
-    MatCardModule,
     MatProgressBarModule,
     MatProgressSpinnerModule,
     FormsModule,
