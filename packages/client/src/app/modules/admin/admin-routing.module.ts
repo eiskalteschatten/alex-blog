7579d2188cf0305b3969c25b25e9a1f2fcdc4aa5
Alex Seifert <alex@alexseifert.com>
2023-10-02 15:52:03 +0200

    Add the Admin component

--- packages/client/src/app/modules/admin/admin-routing.module.ts
+++ packages/client/src/app/modules/admin/admin-routing.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { AdminComponent } from './admin.component';
+
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule),
-  },
-  {
-    path: 'account',
-    loadChildren: () => import('./account/account.module').then(m => m.AccountModule),
+    component: AdminComponent,
+    children: [
+      {
+        path: '',
+        loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule),
+      },
+      {
+        path: 'account',
+        loadChildren: () => import('./account/account.module').then(m => m.AccountModule),
+      },
+    ],
   },
 ];
 
---
+++ packages/client/src/app/modules/admin/admin.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'blog-admin',
+  templateUrl: './admin.component.html',
+  styleUrls: ['./admin.component.scss']
+})
+export class AdminComponent {
+
+}
