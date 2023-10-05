d5e803db37d0123b50d7f684231d3b04b77ab960
Alex Seifert <alex@alexseifert.com>
2023-10-05 16:03:11 +0200

    Cleanup

--- packages/client/src/app/app-routing.module.ts
+++ packages/client/src/app/app-routing.module.ts
@@ -5,11 +5,6 @@ import { adminAuthGuard, authGuard, reverseAuthGuard } from './guards/auth.guard
 
 const routes: Routes = [
   {
-    path: 'account',
-    loadChildren: () => import('./modules/frontend/account/account.module').then(m => m.AccountModule),
-    canActivateChild: [authGuard],
-  },
-  {
     path: 'admin',
     loadChildren: () => import('./modules/admin/admin.module').then(m => m.AdminModule),
     canActivateChild: [adminAuthGuard],
@@ -20,7 +15,7 @@ const routes: Routes = [
     canActivateChild: [reverseAuthGuard],
   },
   {
-    path: '**',
+    path: '',
     loadChildren: () => import('./modules/frontend/frontend.module').then(m => m.FrontendModule),
   },
 ];
