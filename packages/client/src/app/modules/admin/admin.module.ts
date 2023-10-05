98f7b88c685d4b77510daf6aaf74a0d116214f5d
Alex Seifert <alex@alexseifert.com>
2023-10-05 15:54:32 +0200

    Add and use the admin header

--- packages/client/src/app/modules/admin/admin.module.ts
+++ packages/client/src/app/modules/admin/admin.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { AdminHeaderComponent } from '~/layout/admin-header/admin-header.component';
+
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
 
 @NgModule({
   declarations: [
-    AdminComponent
+    AdminComponent,
+    AdminHeaderComponent
   ],
   imports: [
     CommonModule,
