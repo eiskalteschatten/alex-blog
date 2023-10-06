db43a197103bb906ffc0e32c1188b1191e11b01f
Alex Seifert <alex@alexseifert.com>
2023-10-06 12:38:48 +0200

    Started the account menu component

--- packages/client/src/app/shared/account-menu/account-menu.component.ts
+++ packages/client/src/app/shared/account-menu/account-menu.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
+import { AuthService } from '~/services/auth.service';
+
 @Component({
   selector: 'blog-account-menu',
   templateUrl: './account-menu.component.html',
   styleUrls: ['./account-menu.component.scss']
 })
 export class AccountMenuComponent {
+  constructor(
+    private authService: AuthService
+  ) {}
 
+  get userInitials(): string {
+    if (!this.authService.user) {
+      return '';
+    }
+
+    return `${this.authService.user.firstName.charAt(0)}${this.authService.user.lastName.charAt(0)}`.toUpperCase();
+  }
 }
